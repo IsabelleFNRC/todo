87bb3e5440ec3eea162c8fdc270837840955a8cc
IsabelleFNRC <izafatimarodrigues@gmail.com>
2023-05-21 00:24:33 -0400

    adicionando lista

---
+++ src/components/Lista/Lista.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import CartaoTarefa from '../CartaoTarefa/CartaoTarefa.vue'
+
+</script>
+
+<template>
+  <div id="todo-list" class="pl-2">
+    <CartaoTarefa v-for="tarefa in tarefas" :key="tarefa.id" :id-tarefa="tarefa.id" :tarefa="tarefa" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'tarefas'
+  ],
+  created() {
+    console.log(this)
+  }
+}
+</script>
